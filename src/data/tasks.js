e0e018d381b8786a6a50f87d04bc3eb3cb56ede8
Alberto_py <jose.cruzcortes@cesunbc.edu.mx>
2024-02-03 17:44:00 -0800

    Primer Commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Tarea en Proceso",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Tarea 2 por realizar",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Tarea 3 por realizar",
+  },
+  {
+    id: 3,
+    title: "Mi Cuarta tarea",
+    description: "Tarea 4 por realizar",
+  },
+];
